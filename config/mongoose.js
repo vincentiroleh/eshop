422e1128366c819ee82e2e39c7aeb54b5866ad20
Vincent Iroleh <irolehiroleh@gmail.com>
2021-05-02 01:07:58 +0100

    chore:(project stucture) add project dependencies and structure

--- config/mongoose.js
+++ config/mongoose.js
@@ -1,13 +1,13 @@
 const mongoose = require('mongoose');
-const env = require('./env');
+const { DB_URL } = require('./env');
 
 const options = { useUnifiedTopology: true, useNewUrlParser: true };
 
 try {
-    mongoose.connect(env.DB_URL, options)
+    mongoose.connect(DB_URL, options)
     console.log('Database connection initiated')
 } catch (error) {
-    console.log('errorhere', error)
+    console.log(error)
 }
 
 module.exports = mongoose;
